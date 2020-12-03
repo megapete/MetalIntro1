0aa026fae210eb88397dd617c9b387b63c655e66
Peter C Huber <peter@hubersitech.com>
2020-12-03 15:33:11 -0500

    Initial Commit

---
+++ MetalIntro1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MetalIntro1
+//
+//  Created by Peter Huber on 2020-12-03.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
